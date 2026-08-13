b00a20d76b3e34156744090871c86f0aa97ea5a6
Ibrahim Elsadek <iaaelsadek@gmail.com>
2026-08-14 01:37:20 +0300

    style(ui): move menu button more to the right (mr-8)

--- src/components/layout/QuantumNavbar.tsx
+++ src/components/layout/QuantumNavbar.tsx
@@ -67,7 +67,7 @@ export const QuantumNavbar = memo(() => {
         <div className="max-w-[1920px] mx-auto px-4 sm:px-6 md:px-10 lg:px-16 flex items-center justify-between h-16">
 
           {/* Right: Menu + Logo */}
-          <div className="flex items-center gap-3 mr-6">
+          <div className="flex items-center gap-3 mr-8">
             {/* Menu Button */}
             <button
               type="button"
