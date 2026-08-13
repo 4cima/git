d805e34af2f35b141a68f5aa198eacbbeed58ebe
Ibrahim Elsadek <iaaelsadek@gmail.com>
2026-08-14 02:08:34 +0300

    style(ui): increase navbar horizontal padding

--- src/components/layout/QuantumNavbar.tsx
+++ src/components/layout/QuantumNavbar.tsx
@@ -64,7 +64,7 @@ export const QuantumNavbar = memo(() => {
     <>
       {/* Navbar */}
       <nav className="fixed top-0 left-0 right-0 z-[1000] transition-all duration-300 bg-transparent border-b border-transparent">
-        <div className="max-w-[1920px] mx-auto px-1 sm:px-2 md:px-3 lg:px-4 flex items-center justify-between h-16">
+        <div className="max-w-[1920px] mx-auto px-4 sm:px-6 md:px-8 lg:px-12 flex items-center justify-between h-16">
 
           {/* Right: Menu + Logo */}
           <div className="flex items-center gap-8 mr-1">
