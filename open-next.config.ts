ede4676202d82722518f341067377d0cdd270c2d
Ibrahim Elsadek <iaaelsadek@gmail.com>
2026-08-21 04:13:05 +0300

    chore(prod): enable opennext incremental cache via KV + durable object tag cache and queue

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,11 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+import doShardedTagCache  from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
+import doQueue            from "@opennextjs/cloudflare/overrides/queue/do-queue";
 
-export default defineCloudflareConfig();
+export default defineCloudflareConfig({
+  incrementalCache: kvIncrementalCache,
+  tagCache:         doShardedTagCache,
+  queue:            doQueue,
+});
 
